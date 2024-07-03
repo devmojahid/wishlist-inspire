b33b12c6ec6a9fca0eb5d60c078daaea50308440
Mojahid islam <raofahmedmojahid@gmail.com>
2024-07-04 00:49:08 +0600

    all
    Signed-off-by: Mojahid islam <raofahmedmojahid@gmail.com>

--- app/routes/app.deals.jsx
+++ app/routes/app.deals.jsx
@@ -8,30 +8,30 @@ export async function loader({ request }) {
   return json({ deals: await getDeals() });
 }
 
-// export async function action({ request }) {
-//   let settings = await request.formData();
-//   settings = Object.fromEntries(settings);
-//   return json(settings);
-// }
+export async function action({ request }) {
+  let settings = await request.formData();
+  settings = Object.fromEntries(settings);
+  return json(settings);
+}
 
-// export const action = async ({ request }) => {
-//   let formData = new URLSearchParams(await request.text());
-//   let actionType = formData.get("action");
-//   let { createDeal, deleteDeal } = await import("../models/deal.server");
+export const action = async ({ request }) => {
+  let formData = new URLSearchParams(await request.text());
+  let actionType = formData.get("action");
+  let { createDeal, deleteDeal } = await import("../models/deal.server");
 
-//   if (actionType === "create") {
-//     const name = formData.get("name");
-//     const price = formData.get("price");
-//     const startDate = formData.get("startDate");
-//     const endDate = formData.get("endDate");
-//     await createDeal({ name, price, startDate, endDate });
-//   } else if (actionType === "delete") {
-//     const id = formData.get("id");
-//     await deleteDeal(id);
-//   }
+  if (actionType === "create") {
+    const name = formData.get("name");
+    const price = formData.get("price");
+    const startDate = formData.get("startDate");
+    const endDate = formData.get("endDate");
+    await createDeal({ name, price, startDate, endDate });
+  } else if (actionType === "delete") {
+    const id = formData.get("id");
+    await deleteDeal(id);
+  }
 
-//   return redirect("/app/deals");
-// };
+  return redirect("/app/deals");
+};
 
 export default function Deals() {
   const { deals } = useLoaderData();
